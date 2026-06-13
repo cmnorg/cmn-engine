c5d97cb7419b515fd749a9b94c419b4e7209ef23
cmnorg <mikemagloire@cmnorg.org>
2026-06-13 18:41:17 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv(import.meta.env?.MODE || 'production', process.cwd(), '');
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      apiOptions: { region: 'eu' },
+      components: {
+        page: 'storyblok/Page',
+        hero: 'storyblok/Hero',
+        slider: 'storyblok/Slider',
+        slide: 'storyblok/Slide',
+        stats: 'storyblok/Stats',
+        stat: 'storyblok/Stat',
+        campaign: 'storyblok/Campaign',
+        eco_grid: 'storyblok/EcoGrid',
+        eco_card: 'storyblok/EcoCard',
+      },
+    }),
+  ],
+});
